66e3bd0c9c8ae135a8d78b331a8956228931c63d
Jack Dye <jackrdye@gmail.com>
2022-11-17 17:18:11 +0800

    Login UI COmplete

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 
 
@@ -15,22 +14,108 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey,
-      body: Column(
-        children: [
-          // Welcome
+      backgroundColor: Colors.grey[100],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
-          // Text
 
-          // email
+              // Welcome
+              Text(
+                "Hello Again!",
+                style: GoogleFonts.bebasNeue(
+                    fontSize: 36,
+                  ),
+                ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                "Welcome back you've been missed!"
+              ),
+              SizedBox(
+                height: 75,
+              ),
+              
 
-          // Password
+              // Email textfield
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Email",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10)
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
 
-          // Sign in button
 
-          // Register button
-          
-        ],
+              // Password
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(color: Colors.black),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Password",
+                      contentPadding: EdgeInsets.symmetric(horizontal: 10)
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 25,),
+
+
+              // Sign in button
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                child: Container(
+                  padding: EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(10)
+                  ),
+                  child: Center(child: Text("Sign in")),
+                ),
+              ),
+
+
+              // Register button
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Not registered?",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  Text(
+                    " Register here",
+                    style: TextStyle(color: Colors.blue),
+                  )
+                ],
+              )
+
+
+            ],
+          ),
+        ),
       ),
     );
   }
