3cfa59f192b37ee2828260eee15cdaab692af61b
Jack Dye <jackrdye@gmail.com>
2022-11-17 16:08:44 +0800

    Bare login ui

---
+++ lib/login_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Column(
+        children: [
+          // Welcome
+
+          // Text
+
+          // email
+
+          // Password
+
+          // Sign in button
+
+          // Register button
+          
+        ],
+      ),
+    );
+  }
+}
