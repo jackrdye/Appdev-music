9185446cf240e82943460ab05da8714dea1064c8
Jack Dye <jackrdye@gmail.com>
2022-11-17 15:46:57 +0800

    Initial commit - pre built ui

---
+++ lib/main.dart
@@ -0,0 +1,145 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'screens/overview_screen.dart';
+import 'screens/splash_screen.dart';
+import '/screens/auth_screen.dart';
+// import '/screens/overview_screen.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // ignore: deprecated_member_use
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp,
+  ]);
+  await Firebase.initializeApp();
+
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Auth Application',
+        theme: ThemeData(
+            primarySwatch: Colors.pink,
+            backgroundColor: Colors.white,
+            // ignore: deprecated_member_use
+            accentColor: Colors.deepPurple,
+            // ignore: deprecated_member_use
+            accentColorBrightness: Brightness.dark,
+            buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ))),
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (ctx, userSnapshot) {
+            if (userSnapshot.connectionState == ConnectionState.waiting) {
+              return const SplashScreen();
+            }
+            if (userSnapshot.hasData) {
+              return const OverviewScreen();
+            }
+            return const AuthScreen();
+          },
+        ));
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart'
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text("Playlist")
+//         ),
+//         bottomNavigationBar: BottomNavigationBar(
+//           items: const <BottomNavigationBarItem>[
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.home),
+//               label: "Home"
+//             ),
+//             BottomNavigationBarItem(
+//               icon: Icon(Icons.people),
+//               label: "Friends"
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+// class MyWidget extends StatefulWidget {
+//   const MyWidget({super.key});
+
+//   @override
+//   State<MyWidget> createState() => _MyWidgetState();
+// }
+
+// class _MyWidgetState extends State<MyWidget> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container();
+//   }
+// }
+
