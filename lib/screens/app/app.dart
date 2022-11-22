85de8f6497eead814400328360a9e474419a66b0
Jack Dye <jackrdye@gmail.com>
2022-11-23 02:45:22 +0800

    create Playlist, Song, Profile classes for data storage

--- lib/screens/app/app.dart
+++ lib/screens/app/app.dart
@@ -1,9 +1,49 @@
+import 'dart:core';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:music_app/screens/app/friends_page.dart';
 import 'package:music_app/screens/app/home_page.dart';
 import 'package:music_app/screens/app/library_page.dart';
+
+class Playlist {
+  String id;
+  String name;
+  String ownerId;
+  List<Song> songs;
+  List<Profile> collaborators;
+
+  Playlist({required this.id, required this.name, required this.ownerId, required this.songs, required this.collaborators});
+}
+
+class Friend {
+  
+
+}
+
+class Song {
+  String id;
+  String name;
+  String spotifyLink;
+  String appleMusicLink;
+  String youtubeMusicLink;
+
+  Song({required this.id, required this.name, this.spotifyLink="", this.appleMusicLink="", this.youtubeMusicLink=""});
+}
+
+class Profile {
+  String id;
+  String username;
+  String musicService;
+  List<Friend> friends;
+  List<Playlist> playlists;
+  List sharedWithMe;
+
+  Profile({required this.id, required this.username, required this.musicService, required this.friends, required this.playlists, required this.sharedWithMe});
+}
+
+
 
 class App extends StatefulWidget {
   const App({super.key}); 
@@ -14,6 +54,7 @@ class App extends StatefulWidget {
 
 class _AppState extends State<App> {
   final user = FirebaseAuth.instance.currentUser!;
+ 
   int _selectedIndex = 0;
 
   void _navigateBottomBar(index) {
